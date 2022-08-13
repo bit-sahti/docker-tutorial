456524ee7c9de6ff5868f1fe1972532590226fd2
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2022-08-12 21:34:44 -0300

    feat: add database and docker compose config

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,15 @@
 FROM node:14.17-slim 
 
-WORKDIR /home/app
+RUN apt update && apt install -y wget netcat
 
-CMD ["tail", "-f", "/dev/null"]
+RUN wget -q -O /usr/bin/wait-for https://raw.githubusercontent.com/eficode/wait-for/v2.2.3/wait-for && \
+    chmod +x /usr/bin/wait-for
+
+#user 1000 on image - check docker hub image docs
+USER node
+
+WORKDIR /home/app/
+
+COPY package*.json ./
+
+CMD ["/home/app/start.sh"]
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    // service name on docker compose - they share a network by default
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'mydb'
+})
+
+connection.connect()
 
 const app = express()
 
 app.get('/', (_, res) => {
-  res.send('Hello World - from Node.js!')
+  res.send('Hello World - from Node.js! And Full Cycle.')
 })
 
 app.listen(3000, () => {
